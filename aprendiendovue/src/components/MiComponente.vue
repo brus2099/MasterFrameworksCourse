6f781b12503aa00ebe77e60f742a15793a3e1159
@brus2099 <indiebrus@gmail.com>
2020-09-30 23:55:14 -0500

    Traslado de SO. Base de datos funcionando en la nube.

---
+++ aprendiendovue/src/components/MiComponente.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <hr />
+    <h1>A title</h1>
+    <h2>{{ subtitulo }}</h2>
+    <hr />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MiComponente",
+  data() {
+    return {
+      subtitulo: "soy el sub del componente",
+    };
+  },
+};
+</script>
